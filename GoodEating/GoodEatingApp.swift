4fccf589fbddf183abb8b00da230fb691f499947
Deonté Kilgore <deonte.kilgore.dev@gmail.com>
2022-08-21 13:09:34 -0400

    Initial Commit

---
+++ GoodEating/GoodEatingApp.swift
@@ -0,0 +1,17 @@
+//
+//  GoodEatingApp.swift
+//  GoodEating
+//
+//  Created by Deonte Kilgore on 8/21/22.
+//
+
+import SwiftUI
+
+@main
+struct GoodEatingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
