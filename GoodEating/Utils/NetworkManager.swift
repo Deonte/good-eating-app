30ac28b0ce1ec759e235fcc5b97414eb8b990fde
Deonte Kilgore <deonte.kilgore.dev@gmail.com>
2022-10-07 16:39:17 -0400

    added download method to the network manager class

--- GoodEating/Utils/NetworkManager.swift
+++ GoodEating/Utils/NetworkManager.swift
@@ -12,12 +12,12 @@ class NetworkManager: ObservableObject {
     
     enum NetworkError: Error {
         case invalidResponse
+        case responseDecodingFailed
     }
     
     private let session: URLSession
     private let sessionConfiguration: URLSessionConfiguration
-    
-    private let url = "https://foodbukka.herokuapp.com/api/v1/menu"
+    private let url = URL(string: "https://foodbukka.herokuapp.com/api/v1/menu")!
     
     init() {
         self.sessionConfiguration = URLSessionConfiguration.default
@@ -25,7 +25,21 @@ class NetworkManager: ObservableObject {
     }
     
     func downloadItems() async throws {
+        let (menuItems, response) = try await session.data(from: self.url)
+        
+        guard let httpResponse = response as? HTTPURLResponse, httpResponse.statusCode == 200 else {
+            throw NetworkError.invalidResponse
+        }
+        
+        guard let menuResponse = try? JSONDecoder().decode(Response.self, from: menuItems) else {
+            throw NetworkError.responseDecodingFailed
+        }
+        
+        await MainActor.run {
+            items = menuResponse.result
+        }
     }
+    
 }
 
 
