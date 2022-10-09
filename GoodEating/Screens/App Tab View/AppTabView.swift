45c025257cc36f5a554271ea1ff90bbc8d15f57e
Deonte Kilgore <deonte.kilgore.dev@gmail.com>
2022-10-09 19:50:10 -0400

    added comment to showcase animation.

--- GoodEating/Screens/App Tab View/AppTabView.swift
+++ GoodEating/Screens/App Tab View/AppTabView.swift
@@ -36,7 +36,7 @@ struct AppTabView: View {
                 }
             }
             
-            // Splash Screen
+            // Splash Screen : Assignment 1
             ZStack {
                 Color(uiColor: .secondarySystemBackground)
                 
